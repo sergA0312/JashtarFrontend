902ddef2f032dcb4dfc1025ad353648ceafde966
Ra1ymkulov <retype.uun@gmail.com>
2026-05-07 03:03:02 +0600

    add react

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -1,8 +1,16 @@
+"use client";
 import type { FC } from "react";
 import Banner from "./sections/banner/Banner";
 import Events from "../../app/(site)/events/sections/EventsSection/EventsSection";
 import Movement from "./sections/movement/Movement";
+import { useQuery } from "@tanstack/react-query";
+import { getNewsList, newsKeys } from "@/src/entities/news";
 export const HomePage: FC = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: newsKeys.list(),
+    queryFn: getNewsList,
+  });
+  console.log(data);
   return (
     <>
       <Banner />
