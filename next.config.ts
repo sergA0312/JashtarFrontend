b1cc444c6dfff8fec49362d0ff8694f76b2bac42
sergA0312 <117915149+sergA0312@users.noreply.github.com>
2026-05-06 23:41:54 +0600

    Remove eslint config to resolve build issues
    Removed eslint configuration to fix build errors.

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* остальные настройки... */
   typescript: {
-    // ЗАСТАВЛЯЕМ СОБИРАТЬ ДАЖЕ С ОШИБКАМИ
     ignoreBuildErrors: true,
   },
-  eslint: {
-    // На всякий случай игнорим и ошибки линтера
-    ignoreDuringBuilds: true,
-  },
+  // Мы убрали ключ eslint, который вызывал ошибку в логах
 };
 
 export default nextConfig;
